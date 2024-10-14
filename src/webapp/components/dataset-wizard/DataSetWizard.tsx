44499296da55b2b66eed706398cd0410ddad315a
Eduardo Peredo Rivero <jrperedo@gmail.com>
2024-10-14 11:24:29 -0500

    add back button and update i18n

--- src/webapp/components/dataset-wizard/DataSetWizard.tsx
+++ src/webapp/components/dataset-wizard/DataSetWizard.tsx
@@ -1,13 +1,15 @@
-import { Wizard, WizardStep } from "@eyeseetea/d2-ui-components";
-import { Grid, Theme, Typography, createStyles } from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
 import React from "react";
+import { Wizard, WizardStep } from "@eyeseetea/d2-ui-components";
+import { Grid, IconButton, Theme, Typography, createStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
 import i18n from "$/utils/i18n";
 import { IndicatorsDataSet } from "$/webapp/components/dataset-wizard/IndicatorsDataSet";
 import { SetupDataSet } from "$/webapp/components/dataset-wizard/SetupDataSet";
 import { ShareOptionsDataSet } from "$/webapp/components/dataset-wizard/ShareOptionsDataSet";
 import { SummaryDataSet } from "$/webapp/components/dataset-wizard/SummaryDataSet";
+import { useHistory } from "react-router-dom";
 
 export type DataSetWizardProps = { id?: string };
 
@@ -36,12 +38,8 @@ const steps = [
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        root: {
-            paddingBlock: theme.spacing(2),
-        },
-        titleContainer: {
-            padding: theme.spacing(1),
-        },
+        root: { paddingBlock: theme.spacing(2) },
+        titleContainer: { padding: theme.spacing(1) },
     })
 );
 
@@ -51,13 +49,23 @@ export const DataSetWizard = React.memo((props: DataSetWizardProps) => {
     const actionTitle = isEditing ? i18n.t("Edit") : i18n.t("Create");
 
     const classes = useStyles();
+    const history = useHistory();
+
+    const goBackToHome = React.useCallback(() => {
+        history.push("/");
+    }, []);
 
     return (
         <Grid container className={classes.root}>
             <Grid item xs={12} className={classes.titleContainer}>
-                <Typography variant="h5">
-                    {i18n.t("{{action}} a new dataSet", { action: actionTitle })}
-                </Typography>
+                <Grid container alignItems="center" xs={3}>
+                    <IconButton onClick={goBackToHome}>
+                        <ArrowBackIcon />
+                    </IconButton>
+                    <Typography variant="h5">
+                        {i18n.t("{{action}} dataSet", { action: actionTitle })}
+                    </Typography>
+                </Grid>
             </Grid>
             <Grid item xs={12}>
                 <Wizard
