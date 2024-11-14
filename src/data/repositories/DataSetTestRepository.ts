fb2c170a32064a30ac2d7e31770af89011878e2c
Eduardo Peredo Rivero <jrperedo@gmail.com>
2024-11-14 00:11:10 -0500

    update tests

--- src/data/repositories/DataSetTestRepository.ts
+++ src/data/repositories/DataSetTestRepository.ts
@@ -1,11 +1,11 @@
-import { FutureData } from "$/domain/entities/generic/Future";
+import { Future, FutureData } from "$/domain/entities/generic/Future";
 import { DataSet, DataSetList } from "$/domain/entities/DataSet";
 import { Paginated } from "$/domain/entities/Paginated";
 import { DataSetRepository } from "$/domain/repositories/DataSetRepository";
 
 export class DataSetTestRepository implements DataSetRepository {
     getList(): FutureData<Paginated<DataSetList>> {
-        throw new Error("Method not implemented.");
+        return Future.success({ data: [], page: 1, pageCount: 1, total: 0, pageSize: 10 });
     }
     getAll(): FutureData<DataSet[]> {
         throw new Error("Method not implemented.");
